1c0d4b2277dbcc379050c6aa832d07b8481be66e
kaybow <k.bowmaster@hotmail.com>
2016-04-27 21:27:42 -0400

    Fixing Markbot issues

---
+++ js/main.js
@@ -0,0 +1,15 @@
+$('.red-heart').on('click', function () {
+  $('.red-heart').toggleClass('move-heart');
+});
+
+$('.circle1').on('click', function () {
+  $('.circle1').addClass('change-colour');
+});
+
+$('.circle2').on('click', function () {
+  $('.circle2').addClass('change-colour');
+});
+
+$('.circle3').on('click', function () {
+  $('.circle3').addClass('change-colour');
+});
--- js/waypoints.js
+++ js/waypoints.js
@@ -9,18 +9,4 @@ $beigeBox.waypoint(function (direction) {
   }
 }, {offset: (document.documentElement.clientWidth < 590) ? '10%' : '25%' });
 
-$('.red-heart').on('click', function () {
-  $('.red-heart').toggleClass('move-heart');
-});
 
-$('.circle1').on('click', function () {
-  $('.circle1').toggleClass('circle-grow');
-});
-
-$('.circle2').on('click', function () {
-  $('.circle2').toggleClass('circle-grow');
-});
-
-$('.circle3').on('click', function () {
-  $('.circle3').toggleClass('circle-grow');
-});
